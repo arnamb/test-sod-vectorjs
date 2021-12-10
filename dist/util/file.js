4a54560f1294da7f96a277e9e5e00d1a98998aff
arnamb <arnoldas.a20@gmail.com>
2021-12-10 16:48:50 +0200

    Update file.js

--- dist/util/file.js
+++ dist/util/file.js
@@ -59,13 +59,23 @@ exports.getScriptName = getScriptName;
 function download(id, filename) {
     let svg = document.getElementById(id).firstChild;
     let styleSheet = null;
-    for (let i = 0; i < document.styleSheets.length; i++) {
-        // TODO: there is a better way to do this
-        if (document.styleSheets[i] != null && document.styleSheets[i].cssRules[0].cssText.toLowerCase().includes("hatch")) {
-            styleSheet = document.styleSheets[i];
-            break;
+    for (let i = 0; i < document.styleSheets.length; i += 1) {
+        const tempStyleSheet = document.styleSheets[i];
+        if (
+          tempStyleSheet !== null &&
+          tempStyleSheet.href === null &&
+          tempStyleSheet.cssRules !== null
+        ) {
+          if (tempStyleSheet.cssRules[0]) {
+            if (
+              tempStyleSheet.cssRules[0].cssText.toLowerCase().includes('hatch')
+            ) {
+              styleSheet = document.styleSheets[i];
+              break;
+            }
+          }
         }
-    }
+      }
     let style = document.createElementNS('http://www.w3.org/2000/svg', 'style');
     style.type = "text/css";
     let css = "";
